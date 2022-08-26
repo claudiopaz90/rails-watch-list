91711541d83c33a695ab7a5aab0899dbe7187ea0
Claudio Paz <claudio.paz.gonzalez@gmail.com>
2022-08-26 16:10:14 +0200

    Watch List

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,41 +1,34 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: [:new, :create]
-
-  def index
-    @bookmarks = Bookmark.all
-  end
-
   def new
+    # @list =  '/lists/:list_id/bookmarks/new'
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
-    if @bookmark.save
-      redirect_to list_path(@list)
-    else
-      render :new
-    end
-  end
+    @list = List.find(params[:list_id])
+    # @movie = bookmark_param[:movie]
+    # # @comment = bookmark_param[:comment]
+    # @bookmark = Bookmark.new(bookmark_param)
+    # @bookmark.list = @List
+    # @bookmark.save
 
-  def destroy
-    @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    # comment: @comment, movie_id: @movie, list_id: @list
+
+    @bookmark = Bookmark.new
+
+    @params = bookmark_param
+    @bookmark.movie_id = @params[:movie]
+    @bookmark.comment = @params[:comment]
+    @bookmark.list = @list
+    @bookmark.save
+
+    redirect_to list_path(@list)
   end
 
   private
 
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
-  end
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
-  def set_list
-    @list = List.find(params[:list_id])
+  def bookmark_param
+    params.require(:bookmark).permit(:comment, :movie)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   end
 
   def show
-    @bookmark = Bookmark.new
+    @list = List.find(params[:id])
   end
 
   def new
@@ -15,25 +15,13 @@ class ListsController < ApplicationController
 
   def create
     @list = List.create(list_params)
-    if @list.save
-      redirect_to lists_path(@list)
-    else
-      render :new
-    end
-  end
 
-  def destroy
-    @list.destroy
     redirect_to lists_path
   end
 
   private
 
-  def set_list
-    @list = List.find(params[:id])
-  end
-
   def list_params
-    params.require(:list).permit(:name, :photo)
+    params.require(:list).permit(:name)
   end
 end
